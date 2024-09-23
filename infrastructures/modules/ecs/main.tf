c86637831f783a53d8eedde7f1aa775d0052f207
RustamArifin <72663154+tamtamcoding@users.noreply.github.com>
2024-09-23 12:21:38 +0800

    Update main.tf

--- infrastructures/modules/ecs/main.tf
+++ infrastructures/modules/ecs/main.tf
@@ -1,1 +1,106 @@
+# modules/ecs/main.tf
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "ecs_task_role_arn" {
+  description = "ARN of the ECS Task Role"
+  type        = string
+}
+
+variable "ecs_security_group_id" {
+  description = "Security Group ID for ECS"
+  type        = string
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "app-cluster"
+
+  tags = {
+    Name = "app-cluster"
+  }
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "app-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+  task_role_arn      = var.ecs_task_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "app-container"
+      image     = "your-docker-image:latest"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      essential = true
+    }
+  ])
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "app-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.private_subnets
+    security_groups = [var.ecs_security_group_id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = "app-container"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.main.id
+}
+
+output "ecs_service_id" {
+  value = aws_ecs_service.app_service.id
+}
 
