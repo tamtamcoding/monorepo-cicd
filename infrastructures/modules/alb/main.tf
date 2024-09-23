a80449a71b0e0e34590d84b23a6a9ffe1fc57bf9
RustamArifin <72663154+tamtamcoding@users.noreply.github.com>
2024-09-23 12:21:19 +0800

    Update main.tf

--- infrastructures/modules/alb/main.tf
+++ infrastructures/modules/alb/main.tf
@@ -1,1 +1,70 @@
+# modules/alb/main.tf
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security Group ID for ALB"
+  type        = string
+}
+
+resource "aws_lb" "app_lb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_group_id]
+  subnets            = var.public_subnets
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "app-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-299"
+  }
+
+  tags = {
+    Name = "app-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+output "alb_arn" {
+  value = aws_lb.app_lb.arn
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.app_tg.arn
+}
 
